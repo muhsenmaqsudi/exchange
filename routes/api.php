6192b2b6ed69c017d2737df0fa5f83620fb9b3ca
muhsenmaqsudi <muhsenmaqsudi@gmail.com>
2020-09-24 22:58:56 +0330

    installed laravel sanctum package for api authentication

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Validation\ValidationException;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,10 +17,26 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->prefix('users')->group(function () {
+    Route::get('whoami', function (Request $request) {
+        return $request->user();
+    });
+    Route::get('/', [\App\Http\Controllers\UserController::class, 'index']);
 });
 
-Route::middleware('auth:api')->group(function () {
-    Route::get('users', [\App\Http\Controllers\UserController::class, 'index']);
+Route::post('sanctum/token', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+    ]);
+
+    $user = User::where('email', $request->email)->first();
+
+    if (! $user || ! Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+    }
+
+    return $user->createToken('exchange')->plainTextToken;
 });
